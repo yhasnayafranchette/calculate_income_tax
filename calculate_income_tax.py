aa7fa92cc31d9b4586bdd41831b38123c5de2c3b
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-23 00:12:14 +0800

    values on table

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -1,5 +1,12 @@
 #Exercise 12: Calculate income tax for the given income by adhering to the rules below
 
 #Adhering to rules
+taxable_income = 45000
+first_taxable_income = 10000
+next_taxable_income = 20000
+first_rate = 0
+next_rate = 10
+remaining_rate = 20
+
 #Calculate income tax based on the rules
 #Print the result
